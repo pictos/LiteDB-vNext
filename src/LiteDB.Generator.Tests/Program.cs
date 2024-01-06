df78022db0453be527b959921d5d44119339a034
Pedro Jesus <pedrojesus.cefet@gmail.com>
2024-01-06 02:06:26 -0300

    updated SG to be incremental

--- src/LiteDB.Generator.Tests/Program.cs
+++ src/LiteDB.Generator.Tests/Program.cs
@@ -2,10 +2,17 @@
 
 public class Program
 {
-    public static void Main(string[] args)
-    {
+	public static void Main(string[] args)
+	{
 
-    }
+	}
+}
+
+unsafe interface IBla
+{
+	bool ReadPage(byte* page, uint positionID);
+
+	byte* Beta { get; set; }
 }
 
 
@@ -13,39 +20,49 @@ public class Program
 [AutoInterface(typeof(IDisposable))]
 public unsafe class DiskService : IDiskService
 {
-    public DiskService()
-    {
-    }
+	public string? Name { get; set; }
 
-    public void Dispose()
-    {
-        throw new NotImplementedException();
-    }
+	public byte* Beta { get; set; }
 
-    public Stream RendStreamReader()
-    {
-        throw new NotImplementedException();
-    }
+	public DiskService()
+	{
+	}
 
-    public void ReturnReader(Stream stream)
-    {
-    }
+	public virtual void Dispose()
+	{
+		throw new NotImplementedException();
+	}
+
+	public void ReadPage(byte* page, uint positionID)
+	{ 
+	}
+
+	public virtual bool Open(Span<byte> buffer, Stream stream, int length) { return false; }
+
+	//public Stream RendStreamReader()
+	//{
+	//	throw new NotImplementedException();
+	//}
+
+	public void ReturnReader(Stream stream)
+	{
+	}
 }
 
 [AutoInterface]
 public class StreamPool : IStreamPool
 {
-    public StreamPool(int limit)
-    {
-    }
+	public StreamPool(int limit)
+	{
+	}
 
-    public Stream RendStreamReader()
-    {
-        return new MemoryStream();
-    }
+	public Stream RendStreamReader()
+	{
+		return new MemoryStream();
+	}
 
-    public void ReturnReader(Stream stream)
-    {
-    }
+	public void ReturnReader(Stream stream)
+	{
+	}
 
 }
